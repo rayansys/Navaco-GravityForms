550f3f3292abadacc6c7685a610f133125f56448
Alireza Yusefi <rayansys.rs@gmail.com>
2021-12-08 08:37:48 +0330

    init

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: افزونه پرداخت ناواکو فرم ساز Gravity Forms
+Plugin URI: https://navaco.ir
+Description: افزونه درگاه پرداخت ناواکو برای فرم ساز Gravity Forms
+Version: 1.0.0
+Navaco
+Author URI: http://navaco.ir
+*/
+
+if (!defined('ABSPATH')) exit;
+
+require_once('navaco.php');
